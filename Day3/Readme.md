1a0bca800551ded4f78007e808b327a1355b3b0d
amaamina <abouhamrapro@gmail.com>
2025-07-31 10:29:08 +0000

    downcast_pd.py

--- Day3/Readme.md
+++ Day3/Readme.md
@@ -1,8 +1,10 @@
 ### Goals:
 
 #### Part1: Memory Optimization & Chunked Processing for Large Datasets
-- Analyze memory usage of dataframes
-- Optimize memory by changing data types
+- [memory_pd.py](memory_pd.py) -Analyze memory usage of dataframes
+- Optimize memory by changing data types:
+    (Downcasting: Downcasting is changing a data type to a smaller, more specific variant, without loosing information
+    ++ helps reduce memory usage without changing the values or behavior of your data.)
 - Load and process massive files using chunking strategies
 - Apply lazy evaluation techniques for big data
 - Build scalable data ingestion pipelines for preprocessing
---
+++ Day3/downcast_pd.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import numpy as np
+
+#Generate a data frame
+df = pd.DataFrame({
+    'customer_id': np.random.randint(1, 1_000_000, 1_000_000),
+    'age': np.random.randint(18, 90, 1_000_000),
+    'price': np.random.uniform(1, 500, 1_000_000),
+    'city': np.random.choice(['Paris', 'London', 'Berlin', 'Madrid'], 1_000_000),
+    'is_active': np.random.choice([True, False], 1_000_000)
+})
+# Make it inefficient on purpose
+df['is_active'] = df['is_active'].astype(object)
+
+memory_df = df.memory_usage(deep=True).sum()/1024**2
+print (f"original df mem: {memory_df:.2f}MB")
+# Downcasting
+df['customer_id'] = pd.to_numeric(df['customer_id'], downcast='unsigned')
+df['age'] = pd.to_numeric(df['age'], downcast='unsigned')
+df['price'] = pd.to_numeric(df['price'], downcast='float')
+# Convert city to a category
+df['city'] = df['city'].astype('category')
+# Convert is_active to a bool
+df['is_active'] = df['is_active'].astype('bool')
+memory_df = df.memory_usage(deep=True).sum()/1024**2
+print (f"final df mem: {memory_df:.2f}MB")
